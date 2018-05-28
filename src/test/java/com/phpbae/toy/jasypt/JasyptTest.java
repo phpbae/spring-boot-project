93ef276b9891ddb5c6aa3a067a6eab17a9801da7
phpbae <z1004man@naver.com>
2018-05-28 21:51:22 +0900

    [기타] - 프로퍼티 분리(local로 하면 왜 에러 나면서 자동설정이 안되는지 모르겠다 -_-)

--- src/test/java/com/phpbae/toy/jasypt/JasyptTest.java
+++ src/test/java/com/phpbae/toy/jasypt/JasyptTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(properties = "classpath:application-test.properties")
+@SpringBootTest
 public class JasyptTest {
 
     @Ignore
